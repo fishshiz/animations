45ddbb8a824d7bc7c62bcfb591d7cd0cc0172a4b
Edan Lewis <edan@lewisarts.com>
2018-03-06 00:12:36 -0800

    🍊

--- steering/funk/vehicle.js
+++ steering/funk/vehicle.js
@@ -6,17 +6,17 @@ function Vehicle(x, y) {
     this.r = 8;
     this.maxspeed = 6;
     this.maxforce = 1;
-    this.color = (255, 24, 0);
+    this.fleeing = false;
 }
 
 Vehicle.prototype.behaviors = function() {
     var arrive = this.arrive(this.target);
     this.applyForce(arrive);
-
+    this.show();
     var mouse = createVector(mouseX, mouseY);
     var flee = this.flee(mouse);
-    arrive.mult(1);
-    flee.mult(5);
+    // arrive.mult(1);
+    // flee.mult(5);
     this.applyForce(flee);
     this.applyForce(arrive);
 };
@@ -33,8 +33,13 @@ Vehicle.prototype.update = function() {
 };
 
 Vehicle.prototype.show = function() {
-    stroke(Math.floor(Math.random() * Math.floor(255)), Math.floor(Math.random() * Math.floor(255)), Math.floor(Math.random() * Math.floor(255)));
-    strokeWeight(8);
+    if (this.fleeing) {
+        stroke(Math.floor(Math.random() * Math.floor(255)), Math.floor(Math.random() * Math.floor(255)), Math.floor(Math.random() * Math.floor(255)));
+        strokeWeight(12);
+    } else {
+        stroke(225, 104, 30);
+        strokeWeight(8);
+    }
     point(this.pos.x, this.pos.y);
 };
 
@@ -42,10 +47,11 @@ Vehicle.prototype.flee = function(target) {
     var desired = p5.Vector.sub(target, this.pos);
     var d = desired.mag();
     if(d < 50) {
+        this.fleeing = true;
         desired.setMag(this.maxspeed);
         desired.mult(-1);
         var steer = p5.Vector.sub(desired, this.vel);
-        steer.limit(2);
+        steer.limit(15);
         return steer;
     } else {
         return createVector(0, 0);
@@ -55,6 +61,7 @@ Vehicle.prototype.flee = function(target) {
 Vehicle.prototype.arrive = function(target) {
     var desired = p5.Vector.sub(target, this.pos);
     var d = desired.mag();
+    this.fleeing = false;
     var speed = this.maxspeed;
     if (d < 100) {
         speed = map(d, 0, 100, 0, this.maxspeed);
